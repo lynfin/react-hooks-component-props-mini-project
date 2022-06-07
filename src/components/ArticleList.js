3dfbe2c39756e3808282fa1bd28ea675e96d2179
Lyn Finman <lynfinman@gmail.com>
2022-06-07 11:41:02 -0400

    Fleshed out article

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -4,7 +4,7 @@ import Article from "./Article"
 function ArticleList() {
     return (
         <main>
-            <Article key="tmp title" title="tmp title" />
+            <Article key="tmp title" title="tmp title" date="6/7/2022" preview="this is a preview"/>
         </main>
     );
 }
