6abde3e85fc976073a2c0468f21778ece83c7028
Lyn Finman <lynfinman@gmail.com>
2022-06-07 11:38:14 -0400

    Created Article and ArticleList

---
+++ src/components/Article.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Article({title}) {
+  return (
+    <article>
+        <h3>{title}</h3>
+    </article>
+  );
+}
+
+export default Article;
---
+++ src/components/ArticleList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Article from "./Article"
+
+function ArticleList() {
+    return (
+        <main>
+            <Article key="tmp title" title="tmp title" />
+        </main>
+    );
+}
+
+export default ArticleList;
